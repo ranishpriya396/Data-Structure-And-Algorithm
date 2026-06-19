d2c07563b2642945e5cba91b28b8caebe654804d
ranishpriya396 <ranishpriya396@gmail.com>
2026-06-20 02:35:14 +0530

    all edit

--- Array/26_4_Sum_Problem.py
+++ Array/26_4_Sum_Problem.py
@@ -4,15 +4,31 @@ temp= []
 nums.sort()
 n = len(nums)
 for  i in range(0,n):
-    if i < 0 and nums[i] == nums[i-1]:
+    if i > 0 and nums[i] == nums[i-1]:
         continue
     
-    for j in range(j+1,n):
-        if j < 0 and nums[j] == nums[j-1]:
+    for j in range(i+1,n):
+        if j > i+1 and nums[j] == nums[j-1]:
             continue
-    k = j+1 
-    l = n-1
-    while 
+        k = j+1 
+        l = n-1
+        while k < l :
+                total = nums[i]+nums[j]+nums[k]+nums[l]
+                if total < target:
+                    k +=1
+                elif total > target :
+                    l-=1
+                else :
+                    temp.append([nums[i], nums[j], nums[k], nums[l]])
+                    k +=1
+                    l -=1
+                    while k < l and nums[k]==nums[k-1]:
+                        k+=1
+                    while k < l and  nums[l] == nums[l+1]:
+                        l-=1
+
+print(temp)
+        
     
     
     
